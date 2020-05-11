f35925378a95890923d61bc9c983f0dea6bc7afe
Duong Tung Anh <duongtunganh2111@gmail.com>
2020-05-11 13:42:14 +0700

    Update connect.php

--- forum/connect.php
+++ forum/connect.php
@@ -1,7 +1,7 @@
 <?php 
-	$db = mysqli_connect('localhost', 'root', '', 'members');
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "members";
+	$db = mysqli_connect('us-cdbr-east-06.cleardb.net', 'b3861ad067f78e', 'b5660ec4', 'heroku_e6de54bb91c7b51');
+	$servername = "us-cdbr-east-06.cleardb.net";
+	$username = "b3861ad067f78e";
+	$password = "b5660ec4";
+	$dbname = "heroku_e6de54bb91c7b51";
 ?>
