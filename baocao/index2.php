569bdc39cc8dd0fdeb710694de15fd3a80f83d5b
Duong Tung Anh <duongtunganh2111@gmail.com>
2020-05-11 13:49:12 +0700

    Update index2.php

--- baocao/index2.php
+++ baocao/index2.php
@@ -3,7 +3,7 @@
 require_once 'gitConfig.php';
 
 // Include and initialize user class
-require_once 'User.class.php';
+require_once 'user.class.php';
 $user = new User();
 
 if(isset($accessToken)){
