637480118a870445811d31af1adaea6dd18fed79
Duong Tung Anh <duongtunganh2111@gmail.com>
2020-05-11 13:58:42 +0700

    Update server2.php

--- forum/server2.php
+++ forum/server2.php
@@ -1,6 +1,6 @@
 <?php 
 // connect to database
-$db = mysqli_connect('localhost', 'root', '', 'members');
+$db = mysqli_connect('us-cdbr-east-06.cleardb.net', 'b3861ad067f78e', 'b5660ec4', 'heroku_e6de54bb91c7b51');
 
 if (!isset($_SESSION['loggedin'])) {
 	$USER = 'guest';
